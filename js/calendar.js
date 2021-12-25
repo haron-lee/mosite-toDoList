a00371ccf7e2cc70c32c37a40cd7c3b9377bb918
haron-lee <haronxlee@gmail.com>
2021-12-26 00:09:20 +0900

    Add Calendar

--- js/calendar.js
+++ js/calendar.js
@@ -1,4 +1,26 @@
 "use strick";
 
-const calendar = document.querySelector(".calender");
 const calendarText = document.querySelector(".calendar span");
+const dateTest = new Date(Date.UTC(2021, 11, 24));
+
+function getCalendar() {
+  const calendar = new Date();
+  let options = { month: "long" };
+  const WEEKDAY = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const week = WEEKDAY[calendar.getDay()];
+  const year = calendar.getFullYear();
+  const month = calendar.toLocaleDateString("en-Us", options);
+  const date = calendar.getDate();
+
+  calendarText.innerText = `Today is ${week}, ${month} ${date}, ${year}`;
+}
+
+getCalendar();
