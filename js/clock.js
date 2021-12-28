9e3b5b85cefa22e883943a483ae458cd876e6e70
haron-lee <haronxlee@gmail.com>
2021-12-28 23:51:23 +0900

    Add clock

---
+++ js/clock.js
@@ -0,0 +1,17 @@
+const hoursClock = document.querySelector(".hours span");
+const minutesClock = document.querySelector(".minutes span");
+const secondsClock = document.querySelector(".seconds span");
+
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+
+  hoursClock.innerText = hours;
+  minutesClock.innerText = minutes;
+  secondsClock.innerText = seconds;
+}
+
+getClock();
+setInterval(getClock, 1000);
