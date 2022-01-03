a0a211cc6508f51270af11a3bf59d32c41247280
haron-lee <haronxlee@gmail.com>
2022-01-03 19:01:12 +0900

    Add weather forecast

--- js/weather.js
+++ js/weather.js
@@ -2,19 +2,15 @@
 
 const API_KEY = "995e8ef20ddbb04958f309c8dbebc038";
 
-const onGeoSuccess = (position) => {
-  const lat = position.coords.latitude;
-  const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+function getWeatherCurrent(url) {
   fetch(url)
-    .then((response) => response.json())
+    .then((res) => res.json())
     .then((data) => {
       const city = document.querySelector(".weather-text span");
-      const weather = document.querySelector(".weather-main span:first-child");
-      const weatherTemp = document.querySelector(
-        ".weather-main span:last-child"
-      );
+      const weather = document.querySelector(".weather-main");
+      const weatherTemp = document.querySelector(".weather-temp span");
       const weatherIconImg = document.querySelector(".weather-icon img");
+
       const weatherIcon = data.weather[0].icon;
       const weatherIconUrl = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
 
@@ -23,6 +19,54 @@ const onGeoSuccess = (position) => {
       weather.innerText = data.weather[0].main;
       weatherTemp.innerText = `${Math.floor(data.main.temp)}°`;
     });
+}
+
+function getWeatherForecast(url) {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      const weatherForecastBox = document.querySelectorAll(
+        ".weather-forecast_row"
+      );
+      const today = new Date();
+      const forecastData = data.daily;
+
+      for (i = 1; i < forecastData.length; i++) {
+        const forecastBox = weatherForecastBox[i - 1];
+        const dateBox = forecastBox.querySelector(".date");
+        const iconBox = forecastBox.querySelector(".icon");
+        const tempBox = forecastBox.querySelector(".temp");
+        const mainBox = forecastBox.querySelector(".main");
+
+        const date = document.createElement("span");
+        const icon = document.createElement("img");
+        const temp = document.createElement("span");
+        const main = document.createElement("span");
+
+        today.setDate(today.getDate() + 1);
+        date.innerText = `${today.getMonth() + 1}/${today.getDate()}`;
+        icon.src = `http://openweathermap.org/img/wn/${forecastData[i].weather[0].icon}.png`;
+        temp.innerText = `${forecastData[i].temp.min.toFixed()}°/${forecastData[
+          i
+        ].temp.max.toFixed()}°`;
+        main.innerText = `${forecastData[i].weather[0].main}`;
+
+        dateBox.appendChild(date);
+        iconBox.appendChild(icon);
+        tempBox.appendChild(temp);
+        mainBox.appendChild(main);
+      }
+    });
+}
+
+const onGeoSuccess = (position) => {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const currentUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const forecastUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=hourly,minutely&appid=${API_KEY}`;
+
+  getWeatherCurrent(currentUrl);
+  getWeatherForecast(forecastUrl);
 };
 
 const onGeoError = () => {
@@ -30,3 +74,8 @@ const onGeoError = () => {
 };
 
 navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
+
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+// 36.6116044 127.4903614
+// https://api.openweathermap.org/data/2.5/forecast?lat=36.6116044&lon=127.4903614&appid=995e8ef20ddbb04958f309c8dbebc038
+// https://api.openweathermap.org/data/2.5/onecall?lat=36.6116044&lon=127.4903614&units=metric&exclude=hourly,minutely&appid=995e8ef20ddbb04958f309c8dbebc038
